8bbcc9601eeb046dd6c0f0c01fd030dbdc07d3e0
villanela <souththth@usa.com>
2025-07-09 19:13:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function findAndReplace() {
+    // Get user input for height and width
+    const userHeight = document.getElementById("height").value || 150;
+    const userWidth = document.getElementById("width").value || 268;
+
+    // Define multiple words to be replaced and their replacements
+    const replacements = [
+      { find: "</figure>", replace: "" },
+      { find: "gifv", replace: "gif" },
+      { find: 'alt=""', replace: "" },
+      { find: "/> <", replace: "/><" },
+      { find: `<figure data-orig-height="${userHeight}" data-orig-width="${userWidth}">`,replace: ''},
+      { find: `data-orig-height="${userHeight}" data-orig-width="${userWidth}"`, replace: '' },
+      { find: `sizes="(max-width: ${userWidth}px) 100vw, ${userWidth}px"`, replace: '' }, 
+    ];
+
+    // Get the text area
+    const textArea = document.getElementById("textArea");
+    let originalText = textArea.value;
+
+    // Perform replacements
+    replacements.forEach(({ find, replace }) => {
+      originalText = originalText.split(find).join(replace);
+    });
+
+    // Remove 'srcset' attribute using regex
+    originalText = originalText.replace(/\s*srcset="[^"]*"\s*/g, '');
+
+    // Update the text area with the modified content
+    textArea.value = originalText;    
+}
+
+
+
+
+function removeLineBreaks() {
+    // Get the text area
+    const textArea = document.getElementById("textArea");
+
+    // Remove line breaks by replacing all \n and \r\n with empty string
+    textArea.value = textArea.value.replace(/(\r\n|\n|\r)/g, '');  // Removes all line breaks
+}
